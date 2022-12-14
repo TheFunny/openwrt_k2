3a0dff5eed87b32f24ff4de7a584b545f26c51ea
TheFunny <qq11910@126.com>
2022-12-14 20:18:42 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@
  xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
  tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
  cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
- git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/chinadns-ng
- git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+ git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/chinadns-ng
+ git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/luci-app-chinadns-ng
  make info
  make image PROFILE=phicomm_k2-v22.4 PACKAGES="ncat uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap chinadns-ng luci-app-chinadns-ng wpad-openssl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
  #make image PROFILE=lenovo_newifi-y1s PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad-openssl luci-proto-wireguard  luci-app-wireguard curl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
