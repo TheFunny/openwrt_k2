ee34af5a45fdf3768ff4ab2f1be336f8a0d61d5c
TheFunny <qq11910@126.com>
2022-12-14 20:49:05 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,8 +4,30 @@
  xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
  tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
  cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
- git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/chinadns-ng
- git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/luci-app-chinadns-ng
+ #git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/chinadns-ng
+ #git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git packages/luci-app-chinadns-ng
+ 
+ WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+ mkdir $WORKINGDIR -p
+ rm $WORKINGDIR/* -fr
+ wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+ unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+ mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+ rmdir $WORKINGDIR/openwrt-smartdns-master
+ rm $WORKINGDIR/master.zip
+
+ LUCIBRANCH="master" #更换此变量
+ WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+ mkdir $WORKINGDIR -p
+ rm $WORKINGDIR/* -fr
+ wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+ unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+ mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+ rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+ rm $WORKINGDIR/${LUCIBRANCH}.zip
+
+ ./scripts/feeds install -a
+
  make info
- make image PROFILE=phicomm_k2-v22.4 PACKAGES="ncat uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap chinadns-ng luci-app-chinadns-ng wpad-openssl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
+ make image PROFILE=phicomm_k2-v22.4 PACKAGES="ncat uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap smartdns luci-app-smartdns wpad-openssl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
  #make image PROFILE=lenovo_newifi-y1s PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad-openssl luci-proto-wireguard  luci-app-wireguard curl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
