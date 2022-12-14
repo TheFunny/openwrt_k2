f51954c09a27e3b97d4dbb62fdac3d20efeade6c
TheFunny <qq11910@126.com>
2022-12-14 23:02:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -25,9 +25,28 @@
  mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
  rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
  rm $WORKINGDIR/${LUCIBRANCH}.zip
+ 
+ WORKINGDIR="`pwd`/feeds/packages/net/chinadns-ng"
+ mkdir $WORKINGDIR -p
+ rm $WORKINGDIR/* -fr
+ wget https://github.com/NagaseKouichi/openwrt-chinadns-ng-master/archive/master.zip -O $WORKINGDIR/master.zip
+ unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+ mv $WORKINGDIR/openwrt-chinadns-ng-master/* $WORKINGDIR/
+ rmdir $WORKINGDIR/openwrt-chinadns-ng
+ rm $WORKINGDIR/master.zip
+ 
+ WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-chinadns-ng"
+ mkdir $WORKINGDIR -p
+ rm $WORKINGDIR/* -fr
+ wget https://github.com/NagaseKouichi/openwrt-chinadns-ng/archive/luci.zip -O $WORKINGDIR/luci.zip
+ unzip $WORKINGDIR/luci.zip -d $WORKINGDIR
+ mv $WORKINGDIR/openwrt-chinadns-ng-luci/* $WORKINGDIR/
+ rmdir $WORKINGDIR/openwrt-chinadns-ng-luci
+ rm $WORKINGDIR/luci.zip
 
+ ./scripts/feeds update -a
  ./scripts/feeds install -a
 
  make info
- make image PROFILE=phicomm_k2-v22.4 PACKAGES="ncat uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap smartdns luci-app-smartdns wpad-openssl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
+ make image PROFILE=phicomm_k2-v22.4 PACKAGES="ncat uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap smartdns luci-app-smartdns chinadns-ng luci-app-chinadns-ng wpad-openssl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
  #make image PROFILE=lenovo_newifi-y1s PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad-openssl luci-proto-wireguard  luci-app-wireguard curl -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
